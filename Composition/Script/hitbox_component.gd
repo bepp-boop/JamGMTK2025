b2d84d5383dac927a6a57d0f4f51a5f1cac8407e
bepp-boop <quangteoqs15@gmail.com>
2025-07-31 13:16:07 +0200

    Small stuff

--- Composition/Script/hitbox_component.gd
+++ Composition/Script/hitbox_component.gd
@@ -1,4 +1,5 @@
 extends Area2D
+class_name HitBoxComponent
 
 @export var health_component : HealthComponent
 
--- Composition/Script/weapon.gd
+++ Composition/Script/weapon.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
-var attack_damage := 10.0
+var attack_damage_value := 10.0
 var knockback_force := 100.0
+var stun_time := 1.5
 
-
-func _on_hitbox_body_entered(body: Node2D):
-		if body.has_method("damage"):
-			print("Damage Dealt")
-			body.damage(attack_damage)
+func _on_hitbox_body_entered(area):
+	if area is HitBoxComponent:
+		var hitbox : HitBoxComponent = area
+		
+		var attack = Attack.new()
+		attack.attack_damage = attack_damage_value
+		print("Attack")
+		hitbox.damage(attack)
