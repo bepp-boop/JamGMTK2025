8bca11aa43660bfe97aa6f87f1fe99cf744e376f
bepp-boop <quangteoqs15@gmail.com>
2025-08-03 00:25:52 +0200

    edit frame and death frame

--- 5Enviornment3D/3DProps/Breakable/breakable.gd
+++ 5Enviornment3D/3DProps/Breakable/breakable.gd
@@ -5,20 +5,30 @@ var dead = false
 @onready var box: MeshInstance3D = $Box
 @onready var destroy_box: MeshInstance3D = $DestroyedBox
 @onready var audio_stream_player_3d: AudioStreamPlayer3D = $AudioStreamPlayer3D
-@onready var animate = $Sprite2D
+@onready var animate: AnimatedSprite2D = $Sprite2D  # Ensure this is AnimatedSprite2D
 
-
+func _ready():
+	animate.play("idle")  # Play idle animation when spawned
+	
 func kill():
-	print("kill() method triggered")  # Debugging message to confirm it's called
+	if dead:
+		return  # Prevent double-kill
+	print("kill() method triggered")
 	dead = true
-	audio_stream_player_3d.play()  # Play the audio
 	
-	# Delete collision so player can pass through
-	breakable.collision_mask = 0 #delete the collision mask
-	breakable.collision_layer = 0 #delete the collision layer
+	audio_stream_player_3d.play()  # Play death sound
 	
-	print("You can walk past")  # Debugging message after collision is removed
-
-	# Hide or change the mesh visibility
-	box.visible = false  # Hide Full object
-	destroy_box.visible = true  # Show destroyed Object
+	# Disable collisions
+	breakable.collision_mask = 0
+	breakable.collision_layer = 0
+	
+	print("You can walk past")
+	
+	# Change the sprite animation to a permanent death state
+	animate.play("death")  # Make sure this animation exists
+	animate.animation_finished.connect(_on_death_animation_finished)
+	
+func _on_death_animation_finished():
+	# Stop the animation and set it to the last frame
+	animate.stop()
+	animate.frame = animate.sprite_frames.get_frame_count("death") - 1
