7a2ded79e69b02bf34d6a51a5d0ef858fb348b04
AndresR <rengifo00@gmail.com>
2025-08-03 19:51:52 +0200

    change last exit level

---
+++ 0TSCN/lvl_exit_locked.gd
@@ -0,0 +1,51 @@
+extends Node 
+
+
+@onready var switch_manager = get_tree().get_first_node_in_group("switch_manager")
+@onready var area = $AnimatedSprite3D/Area3D
+@export var required_items: Array[String] = ["key"]
+@export var require_all: bool = true  # If false, any one item is enough
+
+
+func _ready():
+	area.body_entered.connect(_on_body_entered)
+	
+
+func _on_body_entered(body: Node):	
+	var character_num = switch_manager.getState()  # Get the active character number (state)
+	print("giving food to baby")
+	var player = get_tree().get_nodes_in_group("player")[character_num]
+	var hasKey = has_required_items(player)
+	if body is CharacterBody3D and body.get_parent().has_method("finish_player") and hasKey:
+		print("You exist")
+		body.get_parent().finish_player(body)
+		print("Player finished their part at the door.")
+
+
+func has_required_items(target: Node) -> bool:
+	if target is CharacterBody3D and target.has_method("has_item"):
+		if require_all:
+			for item in required_items:
+				if not target.has_item(item):
+					return false
+			return true
+		else:
+			for item in required_items:
+				if target.has_item(item):
+					return true
+			return false
+	else:
+		push_warning("Target does not support inventory checking.")
+		return false
+
+
+func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	var character_num = switch_manager.getState()  # Get the active character number (state)
+	var player = get_tree().get_nodes_in_group("player")[character_num]
+	var hasKey = has_required_items(player)
+	print("has Key:%s" % hasKey)
+	if body.get_parent().has_method("finish_player") and hasKey:
+		print("You exist")
+		body.get_parent().finish_player(body)
+		print("Player finished their part at the door.")
+	pass # Replace with function body.
